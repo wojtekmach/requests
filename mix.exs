ce9fdd6da932840dd2705992a1603a03b2eb58a1
Wojtek Mach <wojtek@wojtekmach.pl>
2021-03-18 14:46:48 +0100

    Add ex_doc

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Requests.MixProject do
       version: "0.1.0-dev",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
+      docs: docs(),
       deps: deps()
     ]
   end
@@ -18,13 +19,20 @@ defmodule Requests.MixProject do
     ]
   end
 
+  defp docs do
+    [
+      main: "Requests"
+    ]
+  end
+
   defp deps do
     [
       {:finch, "~> 0.6.0"},
       {:mint, github: "elixir-mint/mint", override: true},
       {:jason, "~> 1.0", optional: true},
       {:nimble_csv, "~> 1.0", optional: true},
-      {:bypass, "~> 2.1", only: :test}
+      {:bypass, "~> 2.1", only: :test},
+      {:ex_doc, ">= 0.0.0", only: :docs}
     ]
   end
 end
